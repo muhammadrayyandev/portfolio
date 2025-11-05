2e7e88b3b0feba33fd75eda5007e80aacfdd7a96
tayyabktk2002 <tayyabktk2002@gmail.com>
2025-11-05 14:07:28 +0500

    next config set for main path

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
